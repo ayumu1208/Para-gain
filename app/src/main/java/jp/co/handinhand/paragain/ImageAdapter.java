f917182b965292526a96184d657ed26021b05b30
Panini <panpanini@users.noreply.github.com>
2021-04-13 23:28:58 +0900

    Add Glide to ImageAdapter

--- app/src/main/java/jp/co/handinhand/paragain/ImageAdapter.java
+++ app/src/main/java/jp/co/handinhand/paragain/ImageAdapter.java
@@ -10,13 +10,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
     private Context mContext;
     private List<Upload> mUplods;
 
-    public  ImageAdapter(MovieFragment context, List<Upload> uploads){
+    public  ImageAdapter(Context context, List<Upload> uploads){
         mContext= context;
         mUplods = uploads;
     }
@@ -32,7 +34,7 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
     public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
         Upload uploadCurrent = mUplods.get(position);
         holder.textViewName.setText(uploadCurrent.getName());
-        with(mContext)
+        Glide.with(mContext)
                 .load(uploadCurrent.getImageUrl())
                 .placeholder(R.mipmap.ic_launcher)
                 .centerCrop()
